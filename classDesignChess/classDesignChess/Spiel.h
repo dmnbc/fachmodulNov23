1ce23e1bbdb07abd36b8c7cb36da456c70112947
Flasher702 <flasher702@gmail.com>
2023-11-24 12:30:14 +0100

    test

--- classDesignChess/classDesignChess/Spiel.h
+++ classDesignChess/classDesignChess/Spiel.h
@@ -11,7 +11,7 @@ using namespace std;
 class Spiel
 {
 private:
-	// das spielfeld hat 64 Felder
+	// das spielfeld hat 64 Felder testy testy
 	//array<array<Feld, 8>, 8> spielfeld;
 	map<string, Feld> spielmap;
 
