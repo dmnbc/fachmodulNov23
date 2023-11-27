09d671c5e3f899a2d6437af5be202fec738008bc
Anton Haustov <anton.haust@gmail.com>
2023-11-27 15:56:14 +0100

    Darstellung von beiden Seiten hinzugefügt, Methode initialisiereFiguren() hinzugefügt

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Feld.laufendeNr = 0;
         Spiel spiel = new Spiel();
+        //Ansicht Spieler Weiß
+        spiel.anzeigen("Weiß");
+        //Ansicht Spieler Schwarz
+        spiel.anzeigen("Schwarz");
 
-        spiel.anzeigen();
     }
 }
