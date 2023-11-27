5387da93f56b8f7e7b931b7b061f97495e108839
Leithamine <131300132+Leithamine@users.noreply.github.com>
2023-11-27 09:27:29 +0100

    Add files via upload

---
+++ csharp/ChessGameConsole/ChessGameConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChessGameConsole
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Willkommen zum Schachspiel!");
+            SchachSpiel spiel = new();
+            spiel.StarteSpiel();
+        }
+    }
+}
