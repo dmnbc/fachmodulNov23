a5cdbe48d49eace2b831fd80c5ca84267ed635c2
Sharina1990 <150929132+Sharina1990@users.noreply.github.com>
2023-11-28 20:55:17 +0100

    Update Feld.cpp

--- gruppe c++/classDesignChess/Feld.cpp
+++ gruppe c++/classDesignChess/Feld.cpp
@@ -1,9 +1,18 @@
 #include "Feld.h"
+#include <string>
+#include "Leer.h"
+#include "Bauer.h"
+#include "Turm.h"
+#include "Koenig.h"
+#include "Dame.h"
+#include "Pferd.h"
+#include "Laeufer.h"
+#include <iostream>
 
 Feld::Feld()
 {
 	feldNr = laufendeNr++;
-	zeile  = feldNr / 8;
+	zeile = feldNr / 8;
 	spalte = feldNr % 8;
 	feldFarbe = (zeile + spalte) % 2 ? 176 : 32;
 	//std::cout << __LINE__ << ":" <<"Feld.cpp -> ctor Feld() "
@@ -14,13 +23,52 @@ Feld::Feld()
 // aus der laufenden Nummer des Feldes ( 0 bis 63 ) kann man das berechnen
 }
 
-Feld::Feld(std::string koordinaten ) // a1 dunkel a2 hell a3 dunkel   
-                    //   [0][1]         b1 hell  c1 dunkel 
-	                //    a  3
+Feld::Feld(std::string koordinaten) // a1 dunkel a2 hell a3 dunkel   
+//   [0][1]         b1 hell  c1 dunkel 
+//    a  3
 {
 	feldNr = laufendeNr++;
-	zeile =  koordinaten[1] -'0';
-	spalte = koordinaten[0] -'b';
-	feldFarbe = (zeile + spalte) % 2 ? 176 : 32;
+	zeile = koordinaten[1] - '0';
+	spalte = koordinaten[0] - 'b';
+	// feldFarbe = (zeile + spalte) % 2 ? 176 : 32;
 	// std::cout << __LINE__ << " feld.cpp " << koordinaten << "\n";
+	// wei� gro� - schwarz klein
+	
+
+	switch (zeile)
+	{
+	case 2:
+	case 7:
+		Bauer('B' + (zeile % 2) * 32);
+		break;
+	case 1:
+	case 8:
+			switch (Off[spalte + 2] - (zeile % 2) * 32)
+			{
+			case 't':
+			case 'T':
+				Turm(Off[spalte + 2] - (zeile % 2) * 32);
+				break;
+			case 'l':
+			case 'L':
+				Laeufer(Off[spalte + 2] - (zeile % 2) * 32);
+				break;
+			case 'p':
+			case 'P':
+				Pferd(Off[spalte + 2] - (zeile % 2) * 32);
+				break;
+			case 'd':
+			case 'D':
+				Dame(Off[spalte + 2] - (zeile % 2) * 32);
+				break;
+			case 'k':
+			case 'K':
+				Koenig(Off[spalte + 2] - (zeile % 2) * 32);
+				break;
+
+			}
+		break;
+	default:
+		Leer();
+	}
 }
