024d7dea9ff6ea6a4cd6a4d97157e913a19e46fc
Sharina1990 <150929132+Sharina1990@users.noreply.github.com>
2023-11-28 10:28:24 +0100

    Add files via upload

---
+++ gruppe c++/classDesignChess/classDesignChess.cpp
@@ -0,0 +1,17 @@
+// classDesignChess.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include "Feld.h"
+#include "Koenig.h"
+#include "Spiel.h"
+#include <iostream>
+
+
+int Feld::laufendeNr = 0;
+
+int main()
+{
+    Spiel s1;
+    s1.anzeigen();
+    //Drehung fehlt noch
+}
