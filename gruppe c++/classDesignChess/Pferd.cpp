a8fc4870d33d8f9172d93315b4f83e8117246e25
Sharina1990 <150929132+Sharina1990@users.noreply.github.com>
2023-11-28 20:41:47 +0100

    Update Pferd.cpp

--- gruppe c++/classDesignChess/Pferd.cpp
+++ gruppe c++/classDesignChess/Pferd.cpp
@@ -1,6 +1,16 @@
 #include "Pferd.h"
+#include <iostream>
 
 Pferd::Pferd()
 {
 	std::cout << __LINE__ << ":" << __FILE__ << "ctor Pferd()\n";
+}
+
+Pferd::Pferd(char Fig)
+{
+	if (Fig == 'p') {
+		std::cout << 'p';
+	}
+	else
+		std::cout << 'P';
 }
