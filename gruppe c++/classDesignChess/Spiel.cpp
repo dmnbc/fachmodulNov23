9f72abbe8e475979f334f10afc444996dedf4c55
PatSC93 <151127925+PatSC93@users.noreply.github.com>
2023-11-29 12:46:44 +0100

    Speicherung von nur einem Feld, muss noch bearbeitet werden

--- gruppe c++/classDesignChess/Spiel.cpp
+++ gruppe c++/classDesignChess/Spiel.cpp
@@ -1,8 +1,11 @@
 #include "Spiel.h"
 #include "Feld.h"
 #include "consolenfarbe.h"
+#include <iostream>
+#include <fstream>
 
 using namespace dkremer;
+using namespace std;
 
 Spiel::Spiel()
 {
@@ -22,41 +25,138 @@ Spiel::Spiel()
 }
 
 void Spiel::anzeigen()
-/*{
- // alle Felder mit ihrer feldFarbe als Quadrat darstellen
-
-	for( auto zeile : spielfeld)
-	{
-		for (auto feld : zeile)
-		{
-			std::cout << feld.get_feldFarbe();
-		}
-		std::cout << "\n";
-	}
-}*/
 {
-	string key;
-	char spalte;
-	char zeile;
-	
-
-	dkremer::concolinit();
-	dkremer::setcolor(green, black);
-	// dkremer::setcolor(red, blue);
-	std::cout << "    A B C D E F G H \n";
-
-	for (zeile = '8'; zeile >= '1'; zeile--)
+	if (richtung)
 	{
-		std::cout << zeile << " |";
-		for (spalte = 'a'; spalte <= 'h'; spalte++)
-		{
-			key = std::string(1, spalte) + zeile;
-			std::cout << ((zeile + spalte) % 2 ? blue : red) << " ";
-			std::cout << spielmap[key].get_figur().get_darstellung();
-		}
+		string key;
+		char spalte;
+		char zeile;
 
-		std::cout << " | " << zeile << "\n";
+
+		dkremer::concolinit();
+		dkremer::setcolor(green, black);
+		// dkremer::setcolor(red, blue);
+		std::cout << "    A B C D E F G H \n";
+
+		for (zeile = '8'; zeile >= '1'; zeile--)
+		{
+			std::cout << zeile << " |";
+			for (spalte = 'a'; spalte <= 'h'; spalte++)
+			{
+				key = std::string(1, spalte) + zeile;
+				std::cout << ((zeile + spalte) % 2 ? blue : red) << " ";
+				std::cout << spielmap[key].get_figur().get_darstellung();
+			}
+
+			std::cout << " | " << zeile << "\n";
+		}
+		std::cout << "    A B C D E F G H \n";
+		dkremer::setcolor(green, black);
+		richtung = false;
 	}
-	std::cout << "    A B C D E F G H \n";
-	dkremer::setcolor(green, black);
+	else
+	{
+		std::cout << "\n";
+
+		string key;
+		char spalte;
+		char zeile;
+
+
+		dkremer::concolinit();
+		dkremer::setcolor(green, black);
+		// dkremer::setcolor(red, blue);
+		std::cout << "    H G F E D C B A \n";
+
+		for (zeile = '1'; zeile <= '8'; zeile++)
+		{
+			std::cout << zeile << " |";
+			for (spalte = 'h'; spalte >= 'a'; spalte--)
+			{
+				key = std::string(1, spalte) + zeile;
+				std::cout << ((zeile + spalte) % 2 ? blue : red) << " ";
+				std::cout << spielmap[key].get_figur().get_darstellung();
+			}
+
+			std::cout << " | " << zeile << "\n";
+		}
+		std::cout << "    H G F E D C B A \n";
+		dkremer::setcolor(green, black);
+		richtung = true;
+	}
 }
+
+
+
+// Wenn input "Speichern", wird diese Methode aufgerufen
+void Spiel::speichern()
+{
+		ofstream speichern;
+		speichern.open("Spielstand.txt");
+
+		if (richtung)
+		{
+			string key;
+			char spalte;
+			char zeile;
+
+
+			dkremer::concolinit();
+			dkremer::setcolor(green, black);
+			// dkremer::setcolor(red, blue);
+			speichern << "    A B C D E F G H \n";
+
+			for (zeile = '8'; zeile >= '1'; zeile--)
+			{
+				speichern << zeile << " |";
+				for (spalte = 'a'; spalte <= 'h'; spalte++)
+				{
+					key = std::string(1, spalte) + zeile;
+					speichern << ((zeile + spalte) % 2 ? blue : red) << " ";
+					speichern << spielmap[key].get_figur().get_darstellung();
+				}
+
+				speichern << " | " << zeile << "\n";
+			}
+			speichern << "    A B C D E F G H \n";
+			dkremer::setcolor(green, black);
+			richtung = false;
+		}
+		else
+		{
+			speichern << "\n";
+
+			string key;
+			char spalte;
+			char zeile;
+
+
+			dkremer::concolinit();
+			dkremer::setcolor(green, black);
+			// dkremer::setcolor(red, blue);
+			speichern << "    H G F E D C B A \n";
+
+			for (zeile = '1'; zeile <= '8'; zeile++)
+			{
+				speichern << zeile << " |";
+				for (spalte = 'h'; spalte >= 'a'; spalte--)
+				{
+					key = std::string(1, spalte) + zeile;
+					speichern << ((zeile + spalte) % 2 ? blue : red) << " ";
+					speichern << spielmap[key].get_figur().get_darstellung();
+				}
+
+				speichern << " | " << zeile << "\n";
+			}
+			speichern << "    H G F E D C B A \n";
+			dkremer::setcolor(green, black);
+			richtung = true;
+		}
+}
+
+// Wenn input "Laden", wird diese Methode aufgerufen
+void Spiel::laden()
+{
+	spielmap = speicherstand;
+}
+	// Spiel wird fortgesetzt durch Verweis auf Spielzugmethodik
